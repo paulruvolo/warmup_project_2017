c08c0dd686ce3e351b61fad73b1b05006be9ef38
Bonnie Ishiguro <bgishiguro@gmail.com>
2017-01-26 18:40:00 -0500

    teleop

--- warmup_project/scripts/teleop.py
+++ warmup_project/scripts/teleop.py
@@ -1,7 +1,13 @@
+from geometry_msgs.msg import Twist
+
 import tty
 import select
 import sys
 import termios
+import rospy
+
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+rospy.init_node('teleop')
 
 def getKey():
     tty.setraw(sys.stdin.fileno())
@@ -10,9 +16,91 @@ def getKey():
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
+def keyToTwist(key):
+    global x
+    global ang
+    global lin_speed
+    global ang_speed
+
+    if key == 'i':
+        x = 1
+        ang = 0
+    elif key == ',':
+        x = -1
+        ang = 0
+    elif key == 'j':
+        x = 0
+        ang = 1
+    elif key == 'l':
+        x = 0
+        ang = -1
+    elif key == 'u':
+        x = 1
+        ang = 1
+    elif key == 'o':
+        x = 1
+        ang = -1
+    elif key == 'm':
+        x = -1
+        ang = 1
+    elif key == '.':
+        x = -1
+        ang = -1
+    
+    elif key == 'w':
+        lin_speed *= 1.1
+    elif key == 'x':
+        lin_speed *= 0.9
+    elif key == 'e':
+        ang_speed *= 1.1
+    elif key == 'c':
+        ang_speed *= 0.9
+
+    else:
+        x = 0
+        ang = 0
+
+    twist = Twist()
+    twist.linear.x = x*lin_speed
+    twist.linear.y = 0
+    twist.linear.z = 0
+    twist.angular.x = 0
+    twist.angular.y = 0
+    twist.angular.z = ang*ang_speed
+    return twist
+
 settings = termios.tcgetattr(sys.stdin)
 key = None
+lin_speed = 0.5
+ang_speed = 1.0
 
-while key != '\x03':
-    key = getKey()
-    print key
+x = 0
+y = 0
+z = 0
+ang = 0
+
+
+
+keypad = """
+
+To move around:
+
+u   i   o
+j   k   l
+m   ,   .
+
+w/x: increase/decrease linear speed by 10%
+e/c: increase/decrease angular speed by 10%
+
+"""
+
+print keypad
+r = rospy.Rate(2)
+while not rospy.is_shutdown():
+    while key != '\x03':
+        key = getKey()
+        twist = keyToTwist(key)
+        pub.publish(twist)
+
+
+print "Teleop is finished."
