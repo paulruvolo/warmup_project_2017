ba73d2cc6c3676fc7e94abe3b18163e1e4264d20
Bonnie Ishiguro <bgishiguro@gmail.com>
2017-01-31 10:52:21 -0500

    person follower process_scan

--- warmup_project/scripts/person_follower.py
+++ warmup_project/scripts/person_follower.py
@@ -4,6 +4,8 @@
 
 import rospy
 import math
+import numpy as np
+from sklearn.cluster import KMeans
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
@@ -12,12 +14,21 @@ class FollowPerson(object):
 		rospy.init_node('person_follow')
 		rospy.Subscriber('/scan', LaserScan, self.process_scan)
 		self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+		self.scan_points = None
+		self.scan_width = 22
 
-	def process_scan(self):
-		pass
+	def process_scan(self, msg):
+		points = np.array([])
+		
+		for i in range(0, self.scan_width):
+			pass
+			# msg.ranges[i]
+		for i in range(359-self.scan_width, 359):
+			pass
+			# msg.ranges[i]
 
 if __name__ == '__main__':
 	node = FollowPerson()
 	print("Node closed.")
 
-
+	# identify closest large object
