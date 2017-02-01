2799ffd47fa47a731c63507f86e6b6f763502517
nathanyee <nathaniel.yee@students.olin.edu>
2017-02-01 00:36:50 -0500

    error sensing seems too low with far away points

--- warmup_project/scripts/wall_detection.py
+++ warmup_project/scripts/wall_detection.py
@@ -6,12 +6,13 @@ from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
 import rospy
 import numpy as np
+import random
 
 class Wall_Detector():
 
     def __init__(self):
         rospy.init_node('wall_detector')
-        rospy.Subscriber('/scan', LaserScan, self.laserCallback)
+        rospy.Subscriber('/stable_scan', LaserScan, self.laserCallback)
 
         self.sleepy = rospy.Rate(2)
         self.angleError = 0
@@ -20,17 +21,33 @@ class Wall_Detector():
     def laserCallback(self, msg):
         print("laserCallback")
 
-        # convert scan data to list of points
-        pts = []
-
         for ang, dist in enumerate(msg.ranges):
             if dist == 0.0:
                 continue
             x = dist * np.cos(ang)
             y = dist * np.sin(ang)
-            pts.append((x, y))
+            try:
+                pts = np.append(pts, [[x,y]], axis=0)
+            except:
+                pts = np.asarray([[x,y]])
 
-        print(np.random.choice(pts, 2, replace=False))
+        np.random.shuffle(pts)
+        a = pts[0]
+        b = pts[1]
+
+        errors = []
+        vecs = []
+
+        for c in pts[2:]:
+            ab = b-a
+            ac = c-a
+            ang = np.arccos(np.dot(ab, ac) / (np.linalg.norm(ab) * np.linalg.norm(ac)))
+            dist = np.linalg.norm(ac) * np.sin(ang)
+            errors.append(dist**2)
+
+        print(errors)
+        print(max(errors))
+        print(sum(errors))
 
 
     def run(self):
