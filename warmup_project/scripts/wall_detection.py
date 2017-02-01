ce755695d51674e85cc767a4e6cf4abce61bcd65
nathanyee <nathaniel.yee@students.olin.edu>
2017-02-01 12:22:03 -0500

    ton of work on wall detection, looking into what is good r^2

--- warmup_project/scripts/wall_detection.py
+++ warmup_project/scripts/wall_detection.py
@@ -5,9 +5,11 @@
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
 import rospy
+
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from scipy import stats
 
 class Wall_Detector():
 
@@ -32,28 +34,41 @@ class Wall_Detector():
             except:
                 pts = np.asarray([[x,y]])
 
-        plt.scatter(pts[:,0],pts[:,1])
-        plt.show()
 
-        np.random.shuffle(pts)
-        a = pts[0]
-        b = pts[1]
-        print(b-a)
+
+        idxs = sorted(random.sample(range(pts.shape[0]), 2))
+
+        a = pts[idxs[0]]
+        b = pts[idxs[1]]
+
+        errorPts = np.asarray([x[1] for x in enumerate(pts) if x[0] not in idxs])
 
         errors = []
-
-        for c in pts[2:]:
+        for c in errorPts:
             ab = b-a
             ac = c-a
             ang = np.arccos(np.dot(ab, ac) / (np.linalg.norm(ab) * np.linalg.norm(ac)))
             dist = np.linalg.norm(ac) * np.sin(ang)
-            errors.append(dist**2)
+            errors.append([c, dist**2])
 
-        print(errors)
-        print(max(errors))
-        print(np.mean(errors))
-        print(sum(errors))
+        x = []
+        y = []
+        for pt, dist in errors:
+            if dist < .1:
+                x.append(pt[0])
+                y.append(pt[1])
 
+        rSquared = (stats.linregress(x, y).rvalue)**2
+        print("wall r^2", rSquared)
+
+        wallPts = [pt[0] for pt in errors if pt[1] < .1]
+        print("wall pts: ", wallPts[0], wallPts[-1])
+        print("pts in wall: ", len(wallPts))
+
+        plt.scatter(pts[:,0],pts[:,1],color='blue')
+        xy = np.transpose(np.asarray([list(a), list(b)]))
+        plt.scatter(xy[0], xy[1],color='red')
+        plt.show()
 
     def run(self):
         while not rospy.is_shutdown():
