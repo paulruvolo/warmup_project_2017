002de5eb5838934a140c0065a1a78bbc0ac621ff
Riley Chapman <riley.chapman@students.olin.edu>
2017-02-04 16:47:50 -0500

    ready to try and run it

--- warmup_project/scripts/obstacle_avoider.py
+++ warmup_project/scripts/obstacle_avoider.py
@@ -16,8 +16,11 @@ class AvoidObstacle(object):
 		self.pub_filtered_points = rospy.Publisher('/filtered_points', Marker, queue_size= 10)
 		self.obstacle_distance = 2.0
 		self.scan_points = []
-		self.propel_force = (10,0) #cartesian, in odom 
-		self.total_force = (0,0) #polar, in base_link
+		self.propel_force = (10,0) #cartesian, in base_link
+		self.total_force = (0,0) #polar, in base_link (rho, phi)
+
+		self.angle_k = 1.0
+		self.linear_k = 1.0
 
 	def process_scan(self, msg):
 		points = []
@@ -43,8 +46,29 @@ class AvoidObstacle(object):
 			point_force = self.cylin_to_cart(angle,-1/distance)
 			total_force[0] += point_force[0] #sum the x and y components of the force
 			total_force[1] += point_force[1]
-		self.total_force = total_force
+		self.total_force = self.cart2pol(total_force)
 
+	def stop(self):
+		twist = Twist()
+		self.pub_cmd_vel.publish(twist)
 
+	def drive(self):
+		twist = Twist()
+		drive_command = self.total_force[0] * self.linear_k
+		turn_command = self.total_force[1] * self.angle_k
 
+		twist.linear.x = drive_command
+		twist.angular.z = turn_command
+		self.pub_cmd_vel.publish(twist)
 
+	def run(self):
+		r = rospy.Rate(20)
+		while not rospy.is_shutdown():
+			self.sum_force()
+			self.drive()
+			r.sleep()
+
+if __name__ == '__main__':
+	node = AvoidObstacle()
+	node.run()
+	print("Node closed.")
